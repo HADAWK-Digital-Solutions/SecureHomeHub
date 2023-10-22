6c7bed891b602bcd5afc9bd7a745cbe23ffd305c
wallbr22 <77262040+wallbr22@users.noreply.github.com>
2023-10-21 19:03:27 -0700

    Add files via upload

---
+++ topology.py
@@ -0,0 +1,25 @@
+import requests
+
+# Configuration
+GATEWAY_URL = "http://your_gateway_ip_address:port"  # replace with your gateway's IP and port
+LIST_DEVICES_ENDPOINT = "/list_devices"  # replace with the appropriate endpoint
+
+def list_connected_devices():
+    """
+    Sends a command to the gateway to list all connected IoT devices.
+    """
+    try:
+        response = requests.get(GATEWAY_URL + LIST_DEVICES_ENDPOINT)
+        
+        if response.status_code == 200:
+            devices = response.json()
+            for device in devices:
+                print(f"Device ID: {device['id']}, Device Name: {device['name']}")
+        else:
+            print(f"Failed to get list of devices. Status Code: {response.status_code}")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    list_connected_devices()
