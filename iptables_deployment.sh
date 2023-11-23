91968a2853b6e549ea5f817b020d959dd291fb84
amr-sait <140765227+amr-sait@users.noreply.github.com>
2023-11-22 19:59:27 -0700

    Update iptables_deployment.sh to match Kyle's changes

--- iptables_deployment.sh
+++ iptables_deployment.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-log() {
-    echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
-}
-
-# Check if the script is run as root
-if [ "$(id -u)" -ne 0 ]; then
-    log "Error: This script must be run as root."
-    exit 1
-fi
-
-log "Flushing existing rules and setting default policies..."
+# Flush existing rules and set default policies
 iptables -F
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
@@ -22,9 +12,6 @@ iptables -A OUTPUT -o lo -j ACCEPT
 
 # Allow established and related connections
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-#This rule will allow traffic from any IP address within the 10.42.0.0/24 subnet to pass through the firewall.
-iptables -A INPUT -s 10.42.0.0/24 -j ACCEPT
 
 # Define a list of IoT device MAC addresses and their preferred hostnames
 declare -A IOT_DEVICES=(
@@ -45,18 +32,6 @@ done
 iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set --name SSH
 iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 4 --name SSH -j DROP
 
-# Allow SSH access for developers (replace the following with the actual IP addresses of your developers)
-# ALLOWED_IPS=("Alec_IP" "Kyle_IP" "Wallace_IP" "Devon_IP" "Habib_IP")
-#for IP in "${ALLOWED_SSH_IPS[@]}"; do
-#    iptables -A INPUT -p tcp --dport 22 -s "$IP" -j ACCEPT
-# done
-
-# Allow RDP access for developers (replace the following with the actual IP addresses of our Developers developers)
-# ALLOWED_IPS=("Alec_IP" "Kyle_IP" "Wallace_IP" "Devon_IP" "Habib_IP")
-# for IP in "${ALLOWED_RDP_IPS[@]}"; do
-#    iptables -A INPUT -p tcp --dport 3389 -s "$IP" -j ACCEPT
-# done
-
 # DROP vs REJECT (REJECT sends a rejection response, DROP silently drops packets)
 # For added security, uncomment the next line to use REJECT for incoming connections
 # iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
@@ -67,37 +42,27 @@ iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --updat
 iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT
 
 # Log dropped packets to a log file
-log "Logging dropped packets..."
 iptables -A INPUT -j LOG --log-prefix "Dropped: " --log-level 7 -m limit --limit 5/m
 
 # Save the rules to persist across reboots
-log "Saving iptables rules..."
 iptables-save > /etc/iptables/SecureHomeHubRule.v4
 
-# Check if ebtables is installed
-if ! command -v ebtables &> /dev/null; then
-    log "Error: ebtables is not installed. Please install ebtables and try again."
-    exit 1
-fi
-
+# ebtables operates by inspecting Ethernet frames as they pass through your Linux bridge.
+# Enabling you to define what type of traffic is permitted or forbidden based on MAC addresses and Ethernet frame fields.
 # Save the ebtables rules
-log "Saving ebtables rules..."
-ebtables-save > /etc/ebtables/SecureHomeHubRule
+#ebtables-save > /etc/ebtables/SecureHomeHubRule
 
 # Make sure ebtables service is enabled and started
-log "Enabling and starting ebtables service..."
-systemctl enable ebtables.service
-systemctl start ebtables.service
+#systemctl enable ebtables.service
+#systemctl start ebtables.service
 
-# Check if Filebeat is installed
+# Install Filebeat if not already installed
 if ! dpkg -l | grep -q "filebeat"; then
-    log "Installing Filebeat..."
     sudo apt-get update
     sudo apt-get install -y filebeat
 fi
 
 # Create a Filebeat configuration for iptables logs
-log "Configuring Filebeat for iptables logs..."
 cat <<EOF > /etc/filebeat/iptables.yml
 filebeat.inputs:
   - type: log
@@ -106,4 +71,8 @@ filebeat.inputs:
       - /var/log/iptables.log  # Update this path to the location of your iptables log file
 
 output.elasticsearch:
-  hosts: ["http://localhost:9200"]  # Update
+  hosts: ["http://localhost:9200"]  # Update this to match your Elasticsearch host and port
+EOF
+
+# Start Filebeat with the custom configuration
+filebeat -e -c /etc/filebeat/iptables.yml
