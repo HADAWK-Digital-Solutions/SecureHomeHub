95e3804ae4228a4eadc7b598ed2281c4b0fcb047
devonwoodman-sait <145609481+devonwoodman-sait@users.noreply.github.com>
2023-11-29 14:06:46 -0700

    Update IoT_devices.py

--- IoT_devices.py
+++ IoT_devices.py
@@ -1,5 +1,4 @@
 import subprocess
-import re
 
 # Define the subnet you want to scan
 subnet = "10.42.0.0/24"
@@ -9,21 +8,16 @@ output_file = "IoT_devices.txt"
 
 # Run the nmap command using subprocess
 try:
-    result = subprocess.run(["sudo", "nmap", "-sn", subnet], capture_output=True, text=True, check=True)
+    result = subprocess.run(
+        f"sudo nmap -v -R -sn -PE -PS80 -PU40,125 {subnet} -oG - | grep 'Status: Up' | awk '{{print \"{{\\\"name\\\": \\\"\"$2\"\\\", \\\"ip\\\": \\\"\"$2\"\\\", \\\"mac\\\":\\\"\"system(\\\"arp -e | grep \"$2\" | awk '{{print $3}}'\\\")\"}}\"}}' > online_devices.txt",
+        shell=True,
+        capture_output=True,
+        text=True,
+        check=True
+    )
     output = result.stdout
-
-    # Use regular expression to extract device name, IP, and MAC address for online hosts
-    pattern = re.compile(r'Nmap scan report for (.+?) \((\d+\.\d+\.\d+\.\d+)\)\s*MAC Address: ((?:[0-9A-Fa-f]{2}[:-]){5}(?:[0-9A-Fa-f]{2}))', re.MULTILINE)
-    matches = pattern.findall(output)
-
-    # Write the extracted information to the output file
     with open(output_file, "w") as file:
-        for match in matches:
-            file.write(f"Device Name: {match[0]}\n")
-            file.write(f"IP Address: {match[1]}\n")
-            file.write(f"MAC Address: {match[2]}\n")
-            file.write("\n")
-
+        file.write(output)
     print(f"Scan results have been written to {output_file}")
 except subprocess.CalledProcessError as e:
     print(f"Error running nmap: {e}")
