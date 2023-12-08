7e796a544b08d7136f24abedb242ae6c36763b66
Kyle <140764654+HADAWK@users.noreply.github.com>
2023-12-07 21:00:19 -0700

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -13,10 +13,29 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
+# Allow SSH/RDPon our port
+iptables -A INPUT -p tcp --dport 49646 -j ACCEPT
+iptables -A INPUT -p tcp --dport 49648 -j ACCEPT
+
 # Blocking default SSH (port 22), RDP (port 3389), and FTP (port 21)
 iptables -A INPUT -p tcp --dport 22 -j DROP
 iptables -A INPUT -p tcp --dport 3389 -j DROP
 iptables -A INPUT -p tcp --dport 21 -j DROP
+
+#Blocking Ports
+iptables -A INPUT -p tcp --dport 23 -j DROP   # Block Telnet
+iptables -A INPUT -p tcp --dport 25 -j DROP   # Block SMTP
+iptables -A INPUT -p udp --dport 161 -j DROP  # Block SNMP
+iptables -A INPUT -p udp --dport 162 -j DROP  # Block SNMP Trap
+iptables -A INPUT -p tcp --dport 135 -j DROP  # Block RPC
+iptables -A INPUT -p udp --dport 137 -j DROP  # Block NetBIOS
+iptables -A INPUT -p udp --dport 138 -j DROP  # Block NetBIOS
+iptables -A INPUT -p tcp --dport 139 -j DROP  # Block NetBIOS
+iptables -A INPUT -p tcp --dport 445 -j DROP  # Block SMB
+iptables -A INPUT -p tcp --dport 1433 -j DROP # Block Microsoft SQL Server
+iptables -A INPUT -p tcp --dport 1434 -j DROP # Block Microsoft SQL Monitor
+iptables -A INPUT -p tcp --dport 69 -j DROP   # Block TFTP
+iptables -A INPUT -p udp --dport 123 -j DROP  # Block NTP
 
 # Allow specific IoT devices by MAC address
 #Tuya Bulb 1
@@ -39,17 +58,18 @@ iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --updat
 iptables -A INPUT -p tcp --dport 49646 -m conntrack --ctstate NEW -m recent --set --name SSH
 iptables -A INPUT -p tcp --dport 49646 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 4 --name SSH -j DROP
 
-# Allow SSH on our port
-iptables -A INPUT -p tcp --dport 49646 -j ACCEPT
-
-# Allow RDP on our port
-iptables -A INPUT -p tcp --dport 49648 -j ACCEPT
-
 # SYN Flood Protection
 iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
 
 # ICMP Protection
 iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT
+
+# Blacklist IP addresses
+# iptables -A INPUT -s [MALICIOUS_IP] -j DROP
+# iptables -A OUTPUT -d [MALICIOUS_IP] -j DROP
+
+# Limit the Number of Parallel Connections per IP
+iptables -A INPUT -p tcp -m connlimit --connlimit-above 50 -j DROP
 
 # Log dropped packets
 iptables -A INPUT -j LOG --log-prefix "Dropped: " --log-level 7 -m limit --limit 5/m
