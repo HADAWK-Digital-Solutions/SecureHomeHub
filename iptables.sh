3babed6e47c89306efa393a56c0cbe5b9c176bda
Kyle <140764654+HADAWK@users.noreply.github.com>
2023-12-07 19:39:57 -0700

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-# Function to add rules for specific services
-#add_service_rule() {
-#    local service_port="$1"
-#    iptables -A INPUT -i wlan0 -p tcp --dport "$service_port" -j ACCEPT
-#    iptables -A FORWARD -i wlan0 -o eth0 -p tcp --dport "$service_port" -j ACCEPT
-#}
-
-iptables -A INPUT -p tcp -s 174.0.243.38 --dport 49646 -j ACCEPT
-iptables -A INPUT -p tcp -s 174.0.243.38 --dport 49648 -j ACCEPT
-
-# Flush existing rules and set default policies
-iptables -F
+# Deny all in/out by default
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
-iptables -P OUTPUT ACCEPT
+iptables -P OUTPUT DROP
 
-# Loopback interface is allowed
+# Allow loopback interface
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
@@ -24,10 +13,50 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# Allow communication between NAT (192.168.50.206) and VLAN (10.42.0.1/24)
-iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT
+# Allow SSH (port 22) from any IP
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
+# Allow RDP (port 3389) from any IP
+iptables -A INPUT -p tcp --dport 3389 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 3389 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+# Allow specific IoT devices by MAC address
+#Tuya Bulb 1
+iptables -A INPUT -m mac --mac-source 50:02:91:57:0A:DA -j ACCEPT
+#Tuya Bulb 2
+iptables -A INPUT -m mac --mac-source 50:02:91:B0:1D:3E -j ACCEPT
+#Tuya Plug
+iptables -A INPUT -m mac --mac-source 50:A0:92:08:70:1C:39 -j ACCEPT
+#Nano Leaf
+iptables -A INPUT -m mac --mac-source 50:00:55:DA:52:62:FE -j ACCEPT
+#Kasa Smart Plug
+iptables -A INPUT -m mac --mac-source 50:78:8C:B5:94:BE:BC -j ACCEPT
+
+# ... and other MAC address rules
+
+# Brute Force Protection for SSH
+iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set --name SSH
+iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 4 --name SSH -j DROP
+
+# DDoS Protection: Rate limiting ICMP
+iptables -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p icmp -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT
+
+# Allow DNS traffic
+iptables -A OUTPUT -p udp --dport 53 -d 192.168.50.1 -j ACCEPT
+iptables -A INPUT -p udp --sport 53 -s 192.168.50.1 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+# Allow HTTP (80) and HTTPS (443) for web browsing
+iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+# Log dropped packets
+iptables -A INPUT -j LOG --log-prefix "Dropped: " --log-level 7 -m limit --limit 5/m
+
+# Save the rules
+iptables-save > /etc/iptables/SecureHomeHubRule.v4
 # Allow specific IoT devices by MAC address
 declare -A IOT_DEVICES=(
     ["50:02:91:57:0A:DA"]="Tuya Bulb 1"
@@ -39,24 +68,3 @@ declare -A IOT_DEVICES=(
     # Add more MAC addresses and hostnames as needed
 )
 
-for MAC in "${!IOT_DEVICES[@]}"; do
-    iptables -A FORWARD -m mac --mac-source "$MAC" -j ACCEPT
-done
-
-# Brute Force Protection (SSH example, adjust as needed)
-iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set --name SSH
-iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 4 --name SSH -j DROP
-
-# DDoS Protection (Example: Rate limiting)
-# Limit incoming ICMP packets (adjust rates as needed)
-iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT
-
-# Add service-specific rules here:
-# Example: Allow MQTT traffic on port 1883
-# add_service_rule 1883
-
-# Log dropped packets to a log file
-iptables -A INPUT -j LOG --log-prefix "Dropped: " --log-level 7 -m limit --limit 5/m
-
-# Save the rules to persist across reboots
-iptables-save > /etc/iptables/SecureHomeHubRule.v4
