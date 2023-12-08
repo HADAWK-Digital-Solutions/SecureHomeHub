e508fc03f46bc8214de9df945d29651859e4161f
Kyle <140764654+HADAWK@users.noreply.github.com>
2023-12-07 20:50:23 -0700

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Deny all in/out by default
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT DROP
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
 
 # Allow loopback interface
 iptables -A INPUT -i lo -j ACCEPT
@@ -13,13 +13,10 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# Allow SSH (port 22) from any IP
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-
-# Allow RDP (port 3389) from any IP
-iptables -A INPUT -p tcp --dport 3389 -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 3389 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+# Blocking default SSH (port 22), RDP (port 3389), and FTP (port 21)
+iptables -A INPUT -p tcp --dport 22 -j DROP
+iptables -A INPUT -p tcp --dport 3389 -j DROP
+iptables -A INPUT -p tcp --dport 21 -j DROP
 
 # Allow specific IoT devices by MAC address
 #Tuya Bulb 1
@@ -32,39 +29,30 @@ iptables -A INPUT -m mac --mac-source 50:A0:92:08:70:1C:39 -j ACCEPT
 iptables -A INPUT -m mac --mac-source 50:00:55:DA:52:62:FE -j ACCEPT
 #Kasa Smart Plug
 iptables -A INPUT -m mac --mac-source 50:78:8C:B5:94:BE:BC -j ACCEPT
-
-# ... and other MAC address rules
+# Additional MAC address rules
 
 # Brute Force Protection for SSH
 iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set --name SSH
 iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 4 --name SSH -j DROP
 
-# DDoS Protection: Rate limiting ICMP
-iptables -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -p icmp -m state --state ESTABLISHED,RELATED -j ACCEPT
+# Brute Force Protection for our SSH port
+iptables -A INPUT -p tcp --dport 49646 -m conntrack --ctstate NEW -m recent --set --name SSH
+iptables -A INPUT -p tcp --dport 49646 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 4 --name SSH -j DROP
+
+# Allow SSH on our port
+iptables -A INPUT -p tcp --dport 49646 -j ACCEPT
+
+# Allow RDP on our port
+iptables -A INPUT -p tcp --dport 49648 -j ACCEPT
+
+# SYN Flood Protection
+iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+
+# ICMP Protection
 iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT
-
-# Allow DNS traffic
-iptables -A OUTPUT -p udp --dport 53 -d 192.168.50.1 -j ACCEPT
-iptables -A INPUT -p udp --sport 53 -s 192.168.50.1 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-
-# Allow HTTP (80) and HTTPS (443) for web browsing
-iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
-iptables -A INPUT -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 # Log dropped packets
 iptables -A INPUT -j LOG --log-prefix "Dropped: " --log-level 7 -m limit --limit 5/m
 
 # Save the rules
-iptables-save > /etc/iptables/SecureHomeHubRule.v4
-# Allow specific IoT devices by MAC address
-declare -A IOT_DEVICES=(
-    ["50:02:91:57:0A:DA"]="Tuya Bulb 1"
-    ["50:02:91:B0:1D:3E"]="Tuya Bulb 2"
-    ["A0:92:08:70:1C:39"]="Tuya Plug"
-    ["00:55:DA:52:62:FE"]="Nano Leaf"
-    ["78:8C:B5:94:BE:BC"]="Kasa Smart Plug"
-    ["48:02:2A:47:AD:4D"]="Netwave IP Camera"
-    # Add more MAC addresses and hostnames as needed
-)
-
+iptables-save > /etc/iptables/rules.v4
